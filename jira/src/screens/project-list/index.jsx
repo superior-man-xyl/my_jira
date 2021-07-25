346b8c5475f6b3b48cf3d0ba67b6cbd9c7592f77
superior-man-xyl <15170725069@163.com>
2021-07-25 23:58:29 +0800

    项目开始，7/25

---
+++ jira/src/screens/project-list/index.jsx
@@ -0,0 +1,11 @@
+import List from "./list"
+import SearchPanel from "./search-panel"
+
+export default projectListScreen=()=>{
+    return (
+        <>
+        <SearchPanel/>
+        <List/>
+        </>
+    )
+}
