af250bb40950ed75a46c48ef4525354b9c3f2414
superior-man-xyl <15170725069@163.com>
2021-07-26 23:34:48 +0800

    7/26号，写项目

--- jira/src/screens/project-list/search-panel.jsx
+++ jira/src/screens/project-list/search-panel.jsx
@@ -1,3 +1,32 @@
-export default SearchPanel=()=>{
-    return <from></from>
+import { useEffect, useState } from "react"
+
+export default SearchPanel = () => {
+    const [param, setparam] = useState({
+        name: '',
+        personId: ''
+    })
+    const [users, setUsers] = useState([]);
+    const [list, setList] = useEffect([]);
+
+    useEffect(() => {
+        fetch('').then(async response => {//待填充URL
+            if (response.ok) {
+                setList(await response.json())
+            }
+        })
+    }, [param])
+    return <from>
+        <div>
+            <input type='text' value={param.name} onChange={(evt) => setparam({
+                ...param,
+                name: evt.target.value
+            })} />
+            <select value={param.personId} onChange={(evt) => setparam({
+                ...param,
+                personId: evt.target.value
+            })} >
+                {users.map(user => <option value={user}>{user.name}</option>)}
+            </select>
+        </div>
+    </from>
 }
